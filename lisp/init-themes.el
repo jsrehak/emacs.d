5f577cc1fbe792b9b3edcb11689b477de6580399
Joshua Rehak <jsrehak@gmail.com>
2015-11-16 13:48:02 -0800

    new theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,9 +1,11 @@
 
 (require-package 'zenburn-theme)
 (require-package 'solarized-theme)
+(require-package 'color-theme-sanityinc-tomorrow)
 
 ;(load-theme 'solarized-dark t)
-(load-theme 'zenburn t)
+;(load-theme 'zenburn t)
+(load-theme 'sanityinc-tomorrow-night t)
 
 ;; set transparency 
 (set-frame-parameter (selected-frame) 'alpha '(85 85))
