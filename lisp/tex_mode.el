6e7954205d3a0901b7a4e9811b018f83aaf0abfb
Joshua Rehak <jsrehak@gmail.com>
2016-08-02 16:52:44 -0600

    updated tex mode to add makeglossaries script

--- lisp/tex_mode.el
+++ lisp/tex_mode.el
@@ -3,3 +3,8 @@
 (turn-on-auto-fill)
 (flyspell-mode)
 (turn-on-reftex)
+
+(eval-after-load "tex"   '(add-to-list 'TeX-command-list
+                                       '("Makeglossaries" "makeglossaries %s" TeX-run-command nil
+                                         (latex-mode)
+                                                  :help "Run makeglossaries script, which will choose xindy or makeindex") t))
