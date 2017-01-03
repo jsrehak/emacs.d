64265383b8a8cdbe9b5608401ab2c07ed140b9fb
Joshua Rehak <jsrehak@gmail.com>
2017-01-03 11:27:47 -0800

    extra package line added

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 
 ;; Startup
 ; Add custom lisp file location
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-packages)
 (require 'init-themes)
