781f164001ba9810242086cb29add3b573eee395
Joshua Rehak <jsrehak@gmail.com>
2017-02-22 17:08:48 -0800

    added hook to LaTex mode to auto insert dollar signs when in math mode

--- lisp/tex_mode.el
+++ lisp/tex_mode.el
@@ -7,4 +7,24 @@
 (eval-after-load "tex"   '(add-to-list 'TeX-command-list
                                        '("Makeglossaries" "makeglossaries %s" TeX-run-command nil
                                          (latex-mode)
-                                                  :help "Run makeglossaries script, which will choose xindy or makeindex") t))
+                                         :help "Run makeglossaries script, which will choose xindy or makeindex") t))
+
+
+(let ((math (reverse (append LaTeX-math-list LaTeX-math-default))))
+  (while math
+    (let ((entry (car math))
+          value)
+      (setq math (cdr math))
+      (if (listp (cdr entry))
+          (setq value (nth 1 entry))
+        (setq value (cdr entry)))
+      (if (stringp value)
+          (fset (intern (concat "LaTeX-math-" value))
+                (list 'lambda (list 'arg) (list 'interactive "*P")
+                      (list 'LaTeX-math-insert value
+                            '(null (texmathp)))))))))
+
+(setq LaTeX-math-list
+      '(
+            (?\ "partial" "Misc Symbol" 8706) ;; #X2202
+        ))
