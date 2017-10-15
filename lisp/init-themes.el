00ded0ba344a449cf8f409d3236eb86b9cbf091a
Joshua Rehak <jsrehak@gmail.com>
2017-10-15 12:01:51 -0700

    removed default transparency behavior

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -9,8 +9,8 @@
 ;(load-theme 'sanityinc-tomorrow-night t)
 
 ;; set transparency 
-(set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85));; set transparency
+;(set-frame-parameter (selected-frame) 'alpha '(85 85))
+;(add-to-list 'default-frame-alist '(alpha 85 85));; set transparency
 
 (defun toggle-transparency ()
   (interactive)
